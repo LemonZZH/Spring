fbd26f8ac4a29a56a0382dbb24832a431027a433
LemonZZH <zzh1315414831>
2016-11-11 10:29:23 +0800

    zzh

---
+++ src/com/example/test/SpringTest.java
@@ -0,0 +1,11 @@
+package com.example.test;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by zzh13 on 2016/11/11.
+ */
+@RequestMapping("/Spring")
+public class SpringTest {
+
+}
